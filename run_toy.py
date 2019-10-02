fba0f415fe2123d1caa44bb75d2d9702fc4ade06
tonyxwz <tonyxwz@yahoo.com>
2019-10-02 16:47:03 +0200

    toymodel
    model are no longer defined using yaml
    but by inheriting from model class

--- run_toy.py
+++ run_toy.py
@@ -2,61 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from pyADAPT import ADAPTapp, DataSet, Model
+from toy_model import ToyModel
 
 
-class ToyModel(Model):
-    @staticmethod
-    def odefunc(t, x, p, u):
-        '''
-        define odes as in natal's github repo. takes t: time, x: states,
-        p: parameters and u: constant inputs as the arguments. return the
-        differential of each state variable.
-        '''
-        u1 = u[0]
-        u2 = u[1]
+toy = ToyModel()
+data = DataSet(raw_data_path='data/toyModel/toyData.npy',
+        data_info_path='data/toyModel/toyData.yaml')
+# print(isinstance(toy, Model))
+app = ADAPTapp(toy, data)
 
-        s1 = x[0]
-        s2 = x[1]
-        s3 = x[2]
-        s4 = x[3]
+# print(toy.constants)
+# print(toy.parameters)
+# print(toy.states)
+# print(toy.observables)
 
-        k1 = p[0]
-        k2 = p[1]
-        k3 = p[2]
-        k4 = p[3]
-        k5 = p[4]
+# app.set_options(dict())
+app.run(n_iter=2)
+# app.set_options(hh=200)
 
-        v1 = k1 * u1 * s2
-        v2 = k2 * u2 * s3
-        v3 = k3 * s1
-        v4 = k4 * s1
-        v5 = k5 * s4
-        ds3dt = v1 - v2
+x0 = toy.states
+tspan = [-100, 0]
+t_eval = np.linspace(*tspan, 100)
 
-        dxdt = np.zeros(len(x))
-        dxdt[0] = v1 - v3 - v4
-        dxdt[1] = -v1 + v2
-        dxdt[2] = ds3dt
-        dxdt[3] = v4 - v5
+y = toy.compute_states(tspan, x0, t_eval=t_eval)
 
-        return dxdt
+import matplotlib.pyplot as plt
 
-D = DataSet(raw_data_path='data/toyModel/toyData.npy',
-    data_info_path='data/toyModel/toyData.yaml')
-spl = D.generate_splines()
-which_plot = 'std'
-# plt.subplot(211)
-plt.plot(spl['s1'][which_plot])
-plt.plot(spl['s2'][which_plot])
-plt.plot(spl['s3'][which_plot])
-plt.plot(spl['s4'][which_plot])
-# plt.legend(['s1', 's2', 's3', 's4'])
+plt.figure(1)
+plt.plot(t_eval, y[0,:])
+plt.plot(t_eval, y[1,:])
+plt.plot(t_eval, y[2,:])
+plt.plot(t_eval, y[3,:])
 
-which_plot = 'value'
-# plt.subplot(212)
-plt.plot(spl['s1'][which_plot])
-plt.plot(spl['s2'][which_plot])
-plt.plot(spl['s3'][which_plot])
-plt.plot(spl['s4'][which_plot])
-plt.legend(['s1', 's2', 's3', 's4'] + [s+"_"+which_plot for s in ['s1', 's2', 's3', 's4']])
+plt.legend(['s1', 's2', 's3', 's4'])
+plt.figure(2)
+plt.plot(t_eval, y[1,:])
 plt.show()
