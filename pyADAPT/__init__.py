e0c973ca70185191e909d9bbce86989a2ea3e030
tonyxwz <tonyxwz@yahoo.com>
2019-10-02 16:36:55 +0200

    add some tasks for later consideration

--- pyADAPT/__init__.py
+++ pyADAPT/__init__.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+TODO:
+add regular simulate along with ADAPT method
+update setup.py to compiled *.pyx into *.pyd
+Monte Carlo module in sampling
+    also Latin Hypercube sampling
+.core.species
+
+"""
+
 from .dataset import DataSet
 from .model import Model
 from .adapt import ADAPTapp
