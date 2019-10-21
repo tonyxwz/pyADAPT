7c9cb6817b70b1f482a346bee8810b6f1252572e
tonyxwz <tonyxwz@yahoo.com>
2019-10-21 11:28:40 +0200

    change toymodel to be imported from pyADAPT

--- tests/test_adapt.py
+++ tests/test_adapt.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
-from pyADAPT import Model, DataSet, ADAPTapp
+from pyADAPT import Model, DataSet, ADAPT
+from pyADAPT.models import ToyModel
 
 
 class Test_ADAPT(unittest.TestCase):
@@ -8,84 +9,7 @@ class Test_ADAPT(unittest.TestCase):
         model = ToyModel()
         data = DataSet(raw_data_path='data/toyModel/toyData.npy',
             data_specs_path='data/toyModel/toyData.yaml')
-        self.app = ADAPTapp(model, data)
+        self.app = ADAPT(model, data)
 
     def test_set_options(self):
         self.assertRaises(Exception, self.app.set_options, hh=120)
-
-    def test_
-
-
-
-class ToyModel(Model):
-    def __init__(self):
-        self.name = "Toy Model"
-        self.description = "Toy Model as appeared in the 2013 paper by Natal van Riel"
-        
-        self.add_predictor(name='t', value=[0, 10])
-
-        self.add_constant('u1', 1)
-        self.add_constant('u2', 1)
-
-        self.add_parameter(name='k1', value=1, vary=True)
-        self.add_parameter(name='k2', value=1, vary=False)
-        self.add_parameter(name='k3', value=0.1, vary=False)
-        self.add_parameter(name='k4', value=0.5, vary=False)
-        self.add_parameter(name='k5', value=1, vary=False)
-
-        self.add_state(name='s1', init=1)
-        self.add_state(name='s2', init=1)
-        self.add_state(name='s3', init=1, observable=False)
-        self.add_state(name='s4', init=1)
-
-        super().__init__()
-    
-    @staticmethod
-    def odefunc(t, x, p, u):
-        """ODE function of the toy model
-        `t`: time
-        `x`: current state
-        `p`: parameters (`self.parameters)
-        `u`: constants (`self.constants`)
-        """
-        v1, v2, v3, v4, v5, ds3dt = ToyModel.reactions(t, x, p, u)
-
-
-        dxdt = np.zeros(len(x))
-        dxdt[0] = v1 - v3 - v4
-        dxdt[1] = -v1 + v2
-        dxdt[2] = ds3dt
-        dxdt[3] = v4 - v5
-
-        return dxdt
-
-    @staticmethod
-    def reactions(t, x, p, u):
-        # TODO: find a way to share the code between `reactions` and `odefunc`
-        u1 = u['u1']
-        u2 = u['u2']
-
-        s1 = x['s1']
-        s2 = x['s2']
-        s3 = x['s3']
-        s4 = x['s4']
-
-        k1 = p['k1']
-        k2 = p['k2']
-        k3 = p['k3']
-        k4 = p['k4']
-        k5 = p['k5']
-
-        v = np.zeros(6)
-        v[0] = k1 * u1 * s2
-        v[1] = k2 * u2 * s3
-        v[2] = k3 * s1
-        v[3] = k4 * s1
-        v[4] = k5 * s4
-        v[5] = v[0] - v[1]
-
-        return v
-
-    def inputs(self, t):
-        # let's skip this for Toy Model
-        return None
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -9,13 +9,16 @@ from pyADAPT.io import read_data_raw
 class Test_DataSet(unittest.TestCase):
     def setUp(self):
         self.npy_path = 'data/toyModel/toyData.npy'
+        self.mat_path = 'data/toyModel/toyData.mat'
         self.info_path = 'data/toyModel/toyData.yaml'
         self.D = DataSet(raw_data_path='data/toyModel/toyData.npy',
-                    data_specs_path='data/toyModel/toyData.yaml')
+                data_specs_path='data/toyModel/toyData.yaml')
 
     def test_read(self):
         result = read_data_raw(self.npy_path)
         self.assertIsInstance(result, np.ndarray)
+        mat_result = read_data_raw(self.mat_path)
+        self.assertIsInstance(mat_result, dict)
 
     def test_interpolate(self):
         n_ts = 187
