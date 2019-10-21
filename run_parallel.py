0f4784d65b21f3d5e20401c7ee8510482c6c1eb7
tonyxwz <tonyxwz@yahoo.com>
2019-10-21 16:49:19 +0200

    fix runtime error:
    on Windows, main module needs __main__ guard to avoid being reimported by subprocesses

--- run_parallel.py
+++ run_parallel.py
@@ -5,10 +5,11 @@ from pyADAPT import ADAPT, DataSet
 from pyADAPT.models import ToyModel
 from logging import DEBUG
 
-toy = ToyModel()
-data = DataSet(raw_data_path='data/toyModel/toyData.mat',
-               data_specs_path='data/toyModel/toyData.yaml')
-# print(isinstance(toy, Model))
-adapt = ADAPT(toy, data)
-adapt.set_options(n_iter=20, n_ts=100, log_level=DEBUG)
-adapt.run()
+if __name__ == '__main__':
+    toy = ToyModel()
+    data = DataSet(raw_data_path='data/toyModel/toyData.mat',
+                data_specs_path='data/toyModel/toyData.yaml')
+    # print(isinstance(toy, Model))
+    adapt = ADAPT(toy, data)
+    adapt.set_options(n_iter=20, n_ts=100)
+    adapt.run(n_core=4)
