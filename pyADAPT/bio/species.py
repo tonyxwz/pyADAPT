1a8fe87dfec8ea7acb7804a657ccddd69384b5f6
Xing <w.xing@student.tue.nl>
2019-12-19 16:23:51 +0100

    a very simple species class

--- pyADAPT/bio/species.py
+++ pyADAPT/bio/species.py
@@ -2,4 +2,5 @@ class Species(object):
     def __init__(self, sp):
         self.id = sp.id
         self.name = sp.name
-        mo
+        self.conc = sp.initial_concentration
+        self.compartment = sp.compartment
