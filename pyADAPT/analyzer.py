3d002f7ffee47dcd5358bdaa1b5f4e7d7845383c
Xing <w.xing@student.tue.nl>
2020-01-17 15:40:46 +0100

    New project components (place holder)

---
+++ pyADAPT/analyzer.py
@@ -0,0 +1,6 @@
+"""analyze the result of ADAPT
+
+a normal model can consist of a number of parameters that is suspected to be inaccurate,
+an analyzer's job is to run ADAPT on all the variants of the model that have only one 
+dynamic parameter, then analyze the parameter trajectories and find the criminal.
+"""
