21b5ee546f0249ad9b7ee1a6ff3c99d4592125c1
tonyxwz <tonyxwz@yahoo.com>
2020-03-12 10:58:07 +0100

    module of functions for plotting ADAPT data

--- pyADAPT/visualize.py
+++ pyADAPT/visualize.py
@@ -1,1 +1,19 @@
+import matplotlib
 import matplotlib.pyplot as plt
+
+
+def create_axes(axes):
+    if axes is None:
+        fig, axes = plt.subplots()
+    else:
+        fig = None
+
+    return fig, axes
+
+
+def plot_error(state, axes=None):
+    # TODO
+    fig, axes = create_axes(axes)
+
+    if fig is not None:
+        plt.show()
