8643a1182cb3567bffc7ae4744a7d7a4886be185
tonyxwz <tonyxwz@yahoo.com>
2020-05-24 22:18:45 +0200

    fix plot titles

--- pyADAPT/trajectory.py
+++ pyADAPT/trajectory.py
@@ -11,12 +11,18 @@ from mpl_toolkits.mplot3d import Axes3D
 
 class Trajectory(dict):
     """Never mind..."""
+
     def __init__(self, data=[], coords=[], name=""):
-        for i_iter in coords['iter']:
+        for i_iter in coords["iter"]:
             data = data[i_iter]
 
     def interp(self, t):
         pass
+
+
+def latexify(s):
+    s = s.split("_")
+    return "$" + s[0] + "_{" + s[1] + "}" + "$"
 
 
 def check_axes(func):
@@ -29,7 +35,7 @@ def check_axes(func):
         axes = np.asarray(axes).flatten()
         assert len(axes) == len(traj.coords["id"])
         for i, x in enumerate(traj.coords["id"]):
-            axes[i].set_title(x.data[()])
+            axes[i].set_title(latexify(x.data[()]))
         func(traj, axes=axes, **kw)
         return axes
 
@@ -60,39 +66,41 @@ def to_matrix(tr: xr.DataArray, size=(50, 50)):
     # v_range shouldn't be simply max and min
     # TODO ptraj is very light because one point is really intense
     v_range = tr.min().item(), tr.max().item()
-    t_range = tr.coords['time'].data[0], tr.coords['time'].data[-1]
-    time = tr.coords['time'].data
+    t_range = tr.coords["time"].data[0], tr.coords["time"].data[-1]
+    time = tr.coords["time"].data
 
     for i_iter in tr.iter:
         line = tr.sel(iter=i_iter.item()).data
         for t, v in zip(time, line):
             x = int(
-                round(((t - t_range[0]) / (t_range[1] - t_range[0])) *
-                      (size[0] - 1)))
+                round(((t - t_range[0]) / (t_range[1] - t_range[0])) * (size[0] - 1))
+            )
             y = int(
-                round(((v - v_range[0]) / (v_range[1] - v_range[0])) *
-                      (size[1] - 1)))
+                round(((v - v_range[0]) / (v_range[1] - v_range[0])) * (size[1] - 1))
+            )
             heatmap[x, y] += 1
     return heatmap
 
 
 @check_axes
-def plot_heatmap(traj: xr.DataArray,
-                 size=(50, 50),
-                 axes=None,
-                 cmap=plt.get_cmap('hot_r'),
-                 **plot_options):
+def plot_heatmap(
+    traj: xr.DataArray,
+    size=(50, 50),
+    axes=None,
+    cmap=plt.get_cmap("hot_r"),
+    **plot_options
+):
     for i, name in enumerate(traj.id):
         tr = traj.sel(id=name)
         # TODO axis ticks
-        axes[i].imshow(to_matrix(tr, size=size).T, origin='lower', cmap=cmap)
+        axes[i].imshow(to_matrix(tr, size=size).T, origin="lower", cmap=cmap)
 
 
 @check_axes
 def plot_3d(traj: xr.DataArray, axes=None, **plot_options):
     if axes is None:
         fig = plt.figure()
-        axes = fig.add_subplot(111, projection='3d')
+        axes = fig.add_subplot(111, projection="3d")
     # TODO plot frequency in 3d axes
     axes.plot_surface()
 
@@ -101,8 +109,7 @@ def plot_3d(traj: xr.DataArray, axes=None, **plot_options):
 def plot(traj: xr.DataArray, axes=None, **plot_options):
     for j, p in enumerate(traj.coords["id"]):
         for i_iter in traj.coords["iter"]:
-            axes[j].plot(time_points(traj), traj.loc[i_iter, :, p],
-                         **plot_options)
+            axes[j].plot(time_points(traj), traj.loc[i_iter, :, p], **plot_options)
 
 
 def save(traj, path):
