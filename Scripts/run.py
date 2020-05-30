048e95e214fbfe92c762e9046f897138e40a79ed
tonyxwz <tonyxwz@yahoo.com>
2020-05-30 13:46:55 +0200

    renaming impects

--- Scripts/run.py
+++ Scripts/run.py
@@ -1,16 +1,16 @@
-import pyADAPT.trajectory as traj
+import pyADAPT.visualize as vis
 import matplotlib.pyplot as plt
 import numpy as np
+from pyADAPT.io import load_traj
 
-
-states = traj.load("s.nc")
-params = traj.load("p.nc")
-fluxes = traj.load("f.nc")
+states = load_traj("s.nc")
+params = load_traj("p.nc")
+fluxes = load_traj("f.nc")
 print(states.shape)
 print(params.shape)
 print(fluxes.shape)
 # fig, axes = plt.subplots(2,3)
-# traj.plot(states, axes=axes, color="red", alpha=0.1)
+# vis.plot(states, axes=axes, color="red", alpha=0.1)
 # plt.show()
 
 fig, axes = plt.subplots()
@@ -20,14 +20,14 @@ correfs = np.zeros(120)
 for iter in range(120):
     correfs[iter] = np.correlate(states[0, :, 0], states[iter, :, 0])
 axes.plot(correfs)
-axes.set_title('Correlation')
+axes.set_title("Correlation")
 
 fig2, axes2 = plt.subplots(2, 2)
 for iter in range(120):
     axes2[(iter // 30) // 2, (iter // 30) % 2].plot(
         states.coords["time"], states[iter, :, 2]
     )
-fig2.suptitle(states.coords['id'][2].data[()])
+fig2.suptitle(states.coords["id"][2].data[()])
 # plt.show()
 
 fig.savefig("fig1.png")
