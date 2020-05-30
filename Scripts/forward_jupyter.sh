8c31bb8c1cf0d725c7f4ab170f620b881efec0d2
tonyxwz <tonyxwz@yahoo.com>
2020-05-30 23:34:32 +0200

    helper scripts to forward jupyter on the cluster to my laptop

---
+++ Scripts/forward_jupyter.sh
@@ -0,0 +1,2 @@
+# run in jumper host
+ssh -N -f -L localhost:12139:localhost:12139 biosim3
---
+++ Scripts/start_jupyter.sh
@@ -0,0 +1,3 @@
+# run in compute-0-2
+export $JUPYTER_LAB_PORT=12139
+jupyter lab --no-browser --port=$JUPYTER_LAB_PORT &
